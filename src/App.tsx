30d7981637c26b12bd280a8eb2b0bbddc7ff371e
Rensir56 <qiran035639@gmail.com>
2025-07-26 16:27:27 +0800

    Eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './Home.tsx'
 import Grid from './Grid.tsx';
 
